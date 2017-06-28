4fdbb9cfd10c9cc59755d1b92819c8ddda6c4033
Josh Cox <josh@webhosting.coop>
2017-06-28 11:34:37 -0500

    README updates, and fixed logdir

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ rm /etc/localtime
 cd /etc; ln -s /usr/share/zoneinfo/$TZ localtime
 if [ ! -z ${DOMOTICZ_OPTS+x} ]
   then
-   /src/domoticz/domoticz -dbase /config/domoticz.db -log/config/domoticz.log -www 8080 -sslwww 0 $DOMOTICZ_OPTS
+   /src/domoticz/domoticz -dbase /config/domoticz.db -log/log/domoticz.log -www 8080 -sslwww 0 $DOMOTICZ_OPTS
 else
  # /usr/local/bin/domoticz -dbase /config/domoticz.db -log/log/domoticz.log -www 8080 -sslwww 0 $DOMOTICZ_OPTS
  # /src/domoticz/domoticz -dbase /config/domoticz.db -log/config/domoticz.log -www 8080 -sslwww 0 $DOMOTICZ_OPTS
-   /src/domoticz/domoticz -dbase /config/domoticz.db -log/config/domoticz.log -www 8080 -sslwww 0
+   /src/domoticz/domoticz -dbase /config/domoticz.db -log /log/domoticz.log -www 8080 -sslwww 0
 fi
